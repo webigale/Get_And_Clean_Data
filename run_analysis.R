d51fee1c7efd707f7ab3aed9895f80a81144a378
webigale <webigale@users.noreply.github.com>
2015-10-20 16:13:10 -0700

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,12 @@
+
+## Merges the training and the test sets to create one data set.
+
+## Extracts the measurements on the mean and standard deviation for each measurement. 
+
+## Uses descriptive activity names to name the activities in the data set.
+
+## Appropriately labels the data set with descriptive variable names. 
+
+## From the data set above, creates a second, independent tidy data set with the
+## average of each variable for each activity and each subject.
+
