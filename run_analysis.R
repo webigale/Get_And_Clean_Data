6d0ed8e188188adace2b30781d9f95c409935bd6
webigale <webigale@users.noreply.github.com>
2015-10-24 15:30:29 -0700

    Complete Step 5

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,8 @@
 ## unzip("getdata-projectfiles-UCI HAR Dataset.zip")
 setwd("UCI HAR Dataset")
 
-## Merges the training and the test sets to create one data set and
-## appropriately labels the data set with descriptive variable names. 
+## Merges the training and the test sets to create one data set. (STEP 1)
+## Appropriately labels the data set with descriptive variable names.  (STEP 4)
 features <- read.table("features.txt")
 feat_lbl <- features$V2
 
@@ -15,7 +15,6 @@ colnames(train_y) <- c("activity")
 train_s <- read.table("train/subject_train.txt")
 colnames(train_s) <- c("subject")
 train_set <- cbind(train_s, train_y, train_x)
-train_set$set <- c("train")
 
 test_x <- read.table("test/X_test.txt")
 colnames(test_x) <- feat_lbl
@@ -24,20 +23,28 @@ colnames(train_y) <- c("activity")
 test_s <- read.table("test/subject_test.txt")
 colnames(test_s) <- c("subject")
 test_set <- cbind(test_s, test_y, test_x)
-test_set$set <- c("test")
 
 data_set <- rbind(test_set, train_set)
 
-## Extracts the measurements on the mean and standard deviation for each measurement. 
+## Extracts the measurements on the mean and standard deviation for each measurement.  (STEP 2)
 mean_fields <- grep("mean[()]", colnames(data_set))
 std_fields <- grep("std[()]", colnames(data_set))
 ## Column 1 is the Subject, column 2 is the activity, and column 564 is the data set.
-short_data <- data_set[, sort(c(1, 2, mean_fields, std_fields, 564))]
+ms_df <- data_set[, sort(c(1, 2, mean_fields, std_fields, 564))]
 
-## Uses descriptive activity names to name the activities in the data set.
+## Uses descriptive activity names to name the activities in the data set. (STEP 3)
 act_lbl <- read.table("activity_labels.txt")
-short_data$activity_name <- as.factor(act_lbl$V2[short_data$activity])
+ms_df$activity_name <- as.factor(act_lbl$V2[ms_df$activity])
 
 ## From the data set above, creates a second, independent tidy data set with the
-## average of each variable for each activity and each subject.
+## average of each variable for each activity and each subject. (STEP 5)
+if(!("dplyr" %in% installed.packages()[,"Package"])) {
+    install.packages("dplyr")
+}
+require(dplyr)
 
+## Convert data frame to a data table
+ms_dt <- tbl_df(ms_df)
+sum_dt <- summarize_each(group_by(ms_dt, subject, activity), funs(mean))
+
+write.table(sum_dt, file = "final.txt", row.names = FALSE)
