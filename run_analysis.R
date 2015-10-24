50c86a9e00e982d4a0138c3c1d6466bd1d37fb63
webigale <webigale@users.noreply.github.com>
2015-10-24 16:47:46 -0700

    made it a function

--- run_analysis.R
+++ run_analysis.R
@@ -2,48 +2,50 @@
 ## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
 ## unzip("getdata-projectfiles-UCI HAR Dataset.zip")
 
+run_analysis <- function () {
 ## Merges the training and the test sets to create one data set. (STEP 1)
 ## Appropriately labels the data set with descriptive variable names.  (STEP 4)
-features <- read.table("UCI HAR Dataset/features.txt")
-feat_lbl <- features$V2
+    features <- read.table("UCI HAR Dataset/features.txt")
+    feat_lbl <- features$V2
 
-train_x <- read.table("UCI HAR Dataset/train/X_train.txt")
-colnames(train_x) <- feat_lbl
-train_y <- read.table("UCI HAR Dataset/train/y_train.txt")
-colnames(train_y) <- c("activity")
-train_s <- read.table("UCI HAR Dataset/train/subject_train.txt")
-colnames(train_s) <- c("subject")
-train_set <- cbind(train_s, train_y, train_x)
+    train_x <- read.table("UCI HAR Dataset/train/X_train.txt")
+    colnames(train_x) <- feat_lbl
+    train_y <- read.table("UCI HAR Dataset/train/y_train.txt")
+    colnames(train_y) <- c("activity")
+    train_s <- read.table("UCI HAR Dataset/train/subject_train.txt")
+    colnames(train_s) <- c("subject")
+    train_set <- cbind(train_s, train_y, train_x)
 
-test_x <- read.table("UCI HAR Dataset/test/X_test.txt")
-colnames(test_x) <- feat_lbl
-test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
-colnames(train_y) <- c("activity")
-test_s <- read.table("UCI HAR Dataset/test/subject_test.txt")
-colnames(test_s) <- c("subject")
-test_set <- cbind(test_s, test_y, test_x)
+    test_x <- read.table("UCI HAR Dataset/test/X_test.txt")
+    colnames(test_x) <- feat_lbl
+    test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
+    colnames(train_y) <- c("activity")
+    test_s <- read.table("UCI HAR Dataset/test/subject_test.txt")
+    colnames(test_s) <- c("subject")
+    test_set <- cbind(test_s, test_y, test_x)
 
-data_set <- rbind(test_set, train_set)
+    data_set <- rbind(test_set, train_set)
 
 ## Extracts the measurements on the mean and standard deviation for each measurement.  (STEP 2)
-mean_fields <- grep("mean[()]", colnames(data_set))
-std_fields <- grep("std[()]", colnames(data_set))
+    mean_fields <- grep("mean[()]", colnames(data_set))
+    std_fields <- grep("std[()]", colnames(data_set))
 ## Column 1 is the Subject and column 2 is the Activity.
-ms_df <- data_set[, sort(c(1, 2, mean_fields, std_fields))]
+    ms_df <- data_set[, sort(c(1, 2, mean_fields, std_fields))]
 
 ## Uses descriptive activity names to name the activities in the data set. (STEP 3)
-act_lbl <- read.table("activity_labels.txt")
-ms_df$activity <- as.factor(act_lbl$V2[ms_df$activity])
+    act_lbl <- read.table("activity_labels.txt")
+    ms_df$activity <- as.factor(act_lbl$V2[ms_df$activity])
 
 ## From the data set above, creates a second, independent tidy data set with the
 ## average of each variable for each activity and each subject. (STEP 5)
-if(!("dplyr" %in% installed.packages()[,"Package"])) {
-    install.packages("dplyr")
-}
-require(dplyr)
+    if(!("dplyr" %in% installed.packages()[,"Package"])) {
+        install.packages("dplyr")
+    }
+    require(dplyr)
 
 ## Convert data frame to a data table
-ms_dt <- tbl_df(ms_df)
-sum_dt <- summarize_each(group_by(ms_dt, subject, activity), funs(mean))
+    ms_dt <- tbl_df(ms_df)
+    sum_dt <- summarize_each(group_by(ms_dt, subject, activity), funs(mean))
 
-write.table(sum_dt, file = "final.txt", row.names = FALSE)
+    write.table(sum_dt, file = "final.txt", row.names = FALSE)
+}
