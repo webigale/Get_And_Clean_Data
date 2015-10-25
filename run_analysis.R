3fd0cbc31b5041189b9a0ad7d244f1b803475f0d
webigale <webigale@users.noreply.github.com>
2015-10-24 17:46:15 -0700

    Updated as function.

--- run_analysis.R
+++ run_analysis.R
@@ -4,37 +4,31 @@
 
 run_analysis <- function () {
 ## Merges the training and the test sets to create one data set. (STEP 1)
-## Appropriately labels the data set with descriptive variable names.  (STEP 4)
-    features <- read.table("UCI HAR Dataset/features.txt")
-    feat_lbl <- features$V2
-
     train_x <- read.table("UCI HAR Dataset/train/X_train.txt")
-    colnames(train_x) <- feat_lbl
     train_y <- read.table("UCI HAR Dataset/train/y_train.txt")
-    colnames(train_y) <- c("activity")
     train_s <- read.table("UCI HAR Dataset/train/subject_train.txt")
-    colnames(train_s) <- c("subject")
-    train_set <- cbind(train_s, train_y, train_x)
+    train_data <- cbind(train_x, train_y, train_s)
 
     test_x <- read.table("UCI HAR Dataset/test/X_test.txt")
-    colnames(test_x) <- feat_lbl
     test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
-    colnames(train_y) <- c("activity")
     test_s <- read.table("UCI HAR Dataset/test/subject_test.txt")
-    colnames(test_s) <- c("subject")
-    test_set <- cbind(test_s, test_y, test_x)
+    test_data <- cbind(test_x, test_y, test_s)
 
-    data_set <- rbind(test_set, train_set)
+    full_data <- rbind(train_data, test_data)
 
 ## Extracts the measurements on the mean and standard deviation for each measurement.  (STEP 2)
-    mean_fields <- grep("mean[()]", colnames(data_set))
-    std_fields <- grep("std[()]", colnames(data_set))
-## Column 1 is the Subject and column 2 is the Activity.
-    ms_df <- data_set[, sort(c(1, 2, mean_fields, std_fields))]
+    features <- read.table("UCI HAR Dataset/features.txt")
+    mean_fields <- grep("mean[()]", features$V2)
+    std_fields <- grep("std[()]", features$V2)
+    ms_data <- full_data[, sort(c(mean_fields, std_fields, 562, 563))]
 
 ## Uses descriptive activity names to name the activities in the data set. (STEP 3)
-    act_lbl <- read.table("activity_labels.txt")
-    ms_df$activity <- as.factor(act_lbl$V2[ms_df$activity])
+    act_lbl <- read.table("UCI HAR Dataset/activity_labels.txt")
+    ms_data$V1.1 <- as.factor(act_lbl$V2[ms_data$V1.1])
+
+## Appropriately labels the data set with descriptive variable names.  (STEP 4)
+    feat_names <- as.vector(features$V2[sort(c(mean_fields, std_fields))])
+    colnames(ms_data) <- c(feat_names, "activity", "subject")
 
 ## From the data set above, creates a second, independent tidy data set with the
 ## average of each variable for each activity and each subject. (STEP 5)
@@ -44,7 +38,7 @@ run_analysis <- function () {
     require(dplyr)
 
 ## Convert data frame to a data table
-    ms_dt <- tbl_df(ms_df)
+    ms_dt <- tbl_df(ms_data)
     sum_dt <- summarize_each(group_by(ms_dt, subject, activity), funs(mean))
 
     write.table(sum_dt, file = "final.txt", row.names = FALSE)
